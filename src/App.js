85fe3a2752db73341c5c7af48a5b9205ea22a96b
YERIM <dnaer456@gmail.com>
2020-07-20 21:43:51 +0900

    firebase에서 key값 갖고오기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
-import React from "react";
+import React, { memo } from "react";
+import axios from "axios";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      memoData: null,
+    };
+  }
+  componentDidMount = async () => {
+    const data1 = "Hi";
+    const data2 = "라이언";
+
+    const inputData = {
+      data1: data1,
+      data2: data2,
+    };
+    /* Client가 Backend 에게 data보내줄려고 axios를 만듬 */
+    await axios
+      .post(
+        "/api/test",
+        {
+          params: { inputData },
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) =>
+        this.setState({
+          memoData: response.data,
+        })
+      );
+  };
+
   render() {
-    return <div>Hello react</div>;
+    const { memoData } = this.state;
+    return (
+      <div>
+        {memoData === null
+          ? "Loding..."
+          : memoData.map((memo) => {
+              return (
+                <div key={memo.refKey}>
+                  <h1>{memo.title}</h1>
+                  <h3>{memo.content}</h3>
+                  <h4>{memo.regDate}</h4>
+                </div>
+              );
+            })}
+      </div>
+    );
   }
 }
 export default App;
